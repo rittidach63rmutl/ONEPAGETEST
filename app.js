0c1c01db6950f47b37704a5f711d442bfe64a0ee
rittidach63rmutl <rittidachtaya63@gmail.com>
2026-06-23 23:50:18 +0700

    add new project01

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const chalk = require('chalk')
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();  
+const port = 3000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.get("/",(req,res)=>{
+
+    res.send('Hello bookDEV.co.th');
+})
+
+app.listen(port,()=>{
+
+        debug("Listening on port" + chalk.red(" : "+port));
+
+})
+
