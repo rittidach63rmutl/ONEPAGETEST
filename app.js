fda1a3b5e526634aeaf9ca53fbb456b8d8cc3e21
rittidach63rmutl <rittidachtaya63@gmail.com>
2026-06-29 23:14:08 +0700

    edti publice

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const produtRouter = express.Router();
 
 const app = express();  
 const PORT = process.env.PORT || 4000;
@@ -12,6 +13,19 @@ app.use(express.static(path.join(__dirname,"/public/")));
 
 app.set("views","./src/views");
 app.set("view engine","ejs")
+
+produtRouter.route("/").get((req,res)=>{
+    res.render("products",{
+        products:[
+            {productsTitle:'น้ำยาล้างจาน',productsDescription: 'น้ำยาล้างจานสูตร 1 ดีเลิศ',productsPrice: 45},
+            {productsTitle:'น้ำยาล้างจาน 2',productsDescription: 'น้ำยาล้างจานสูตร 2 ดีมาก',productsPrice: 40},
+            {productsTitle:'น้ำยาล้างจาน 3',productsDescription: 'น้ำยาล้างจานสูตร 3 ดีมั้ง',productsPrice: 35},
+            {productsTitle:'น้ำยาล้างจาน 4',productsDescription: 'น้ำยาล้างจานสูตร 4 ดีเฉยๆ',productsPrice: 30},
+        ],
+    });
+});
+
+app.use("/products",produtRouter)
 
 app.get("/",(req,res)=>{
 
